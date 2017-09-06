507e6d08a71be3d2e0f872da88583eb1bb5d8eb6
Binbin Qian <qianbinbin@longcheer.com>
2017-09-06 14:36:56 +0800

    update

--- src/main/java/io/binac/factory/simplefactory/SimpleFactoryTest.java
+++ src/main/java/io/binac/factory/simplefactory/SimpleFactoryTest.java
@@ -1,12 +1,12 @@
 package io.binac.factory.simplefactory;
 
 import io.binac.factory.IProduct;
-import io.binac.factory.simplefactory.SimpleFactory.ProductType;
+import io.binac.factory.simplefactory.ConcreteSimpleFactory.ProductType;
 
 public class SimpleFactoryTest {
     public static void main(String[] args) {
         ProductType type = ProductType.TYPE_A;
-        IProduct product = SimpleFactory.createProduct(type);
+        IProduct product = ConcreteSimpleFactory.createProduct(type);
         product.use();
     }
 }
