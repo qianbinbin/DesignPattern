23cc57301572e1685f20f8fe00eb8215b39454b8
Binbin Qian <qianbinbin@longcheer.com>
2017-09-07 11:26:04 +0800

    update

--- src/main/java/io/binac/factory/abstractfactory/AbstractFactoryTest.java
+++ src/main/java/io/binac/factory/abstractfactory/AbstractFactoryTest.java
@@ -3,8 +3,8 @@ package io.binac.factory.abstractfactory;
 public class AbstractFactoryTest {
     public static void main(String[] args) {
         AbstractFactory factory1 = new ConcreteFactory1();
-        IProductA productA = factory.createProductA();
-        IProductB productB = factory.createProductB();
+        IProductA productA = factory1.createProductA();
+        IProductB productB = factory1.createProductB();
         productA.use();
         productB.use();
     }
